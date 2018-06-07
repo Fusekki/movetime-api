fd793df2ad06c629c87590d2bcb2c0857431e544
Phillip Stafford <phillip@philliprstafford.com>
2018-06-07 15:26:36 -0400

    refactor: Add wildcard to origin in corsOptions var

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ MongoClient.connect(db.url, (err, database) => {
 // });
 
 var corsOptions = {
-  origin: 'http://example.com',
+  origin: '*',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
 }
 
