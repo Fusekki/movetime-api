36aff6649f9d9e5c38f01759a17dd004f81c8a7f
Phillip Stafford <phillip@philliprstafford.com>
2018-06-07 15:26:52 -0400

    feat: Begin adding get for all users

--- app/routes/movietime_routes.js
+++ app/routes/movietime_routes.js
@@ -2,6 +2,23 @@ var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function (app, db) {
 
+    app.get('/users', (req, res) => {
+        const id = req.params.id;
+        const details = {
+            '_id': new ObjectID(id)
+        };
+        db.collection('movietimes').find({}, (err, item) => { _.toArray()
+            if (err) {
+                res.send({
+                    'error': 'An error has occurred'
+                });
+            } else {
+                res.send(item);
+            }
+        });
+
+    });
+
     app.get('/users/:id', (req, res) => {
         const id = req.params.id;
         const details = {
