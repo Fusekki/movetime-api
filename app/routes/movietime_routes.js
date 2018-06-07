6a1f3a7cd7e7f55444d6370120fcc6df804f33c4
Phillip Stafford <phillip@philliprstafford.com>
2018-06-07 14:35:46 -0400

    feat: Add mogodb and starter get and post routes for user

---
+++ app/routes/movietime_routes.js
@@ -0,0 +1,31 @@
+var ObjectID = require('mongodb').ObjectID;
+
+module.exports = function(app, db) {
+
+    app.get('/users/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('movietimes').findOne(details, (err, item) => {
+            if (err) {
+            res.send({'error':'An error has occurred'});
+            } else {
+            res.send(item);
+            }
+        });
+    
+    });
+    app.post('/users', (req, res) => {
+        // console.log(req.body);
+        // res.send('Hello')
+        const user = { name: req.body.name, email: req.body.email };
+        db.collection('movietimes').insert(user, (err, result) => {
+          if (err) { 
+            res.send({ 'error': 'An error has occurred' }); 
+          } else {
+            res.send(result.ops[0]);
+          }
+        });
+    });
+
+
+};
