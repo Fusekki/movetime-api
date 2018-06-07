a943cd49430be7de348479280a873aaaceea0091
Phillip Stafford <phillip@philliprstafford.com>
2018-06-07 14:44:44 -0400

    style: Beautify

--- app/routes/movietime_routes.js
+++ app/routes/movietime_routes.js
@@ -1,44 +1,57 @@
 var ObjectID = require('mongodb').ObjectID;
 
-module.exports = function(app, db) {
+module.exports = function (app, db) {
 
     app.get('/users/:id', (req, res) => {
         const id = req.params.id;
-        const details = { '_id': new ObjectID(id) };
+        const details = {
+            '_id': new ObjectID(id)
+        };
         db.collection('movietimes').findOne(details, (err, item) => {
             if (err) {
-            res.send({'error':'An error has occurred'});
+                res.send({
+                    'error': 'An error has occurred'
+                });
             } else {
-            res.send(item);
+                res.send(item);
             }
         });
-    
+
     });
 
     app.post('/users', (req, res) => {
         // console.log(req.body);
         // res.send('Hello')
-        const user = { name: req.body.name, email: req.body.email };
+        const user = {
+            name: req.body.name,
+            email: req.body.email
+        };
         db.collection('movietimes').insert(user, (err, result) => {
-          if (err) { 
-            res.send({ 'error': 'An error has occurred' }); 
-          } else {
-            res.send(result.ops[0]);
-          }
+            if (err) {
+                res.send({
+                    'error': 'An error has occurred'
+                });
+            } else {
+                res.send(result.ops[0]);
+            }
         });
     });
 
     app.delete('/users/:id', (req, res) => {
         const id = req.params.id;
-        const details = { '_id': new ObjectID(id) };
+        const details = {
+            '_id': new ObjectID(id)
+        };
         db.collection('movietimes').remove(details, (err, item) => {
-          if (err) {
-            res.send({'error':'An error has occurred'});
-          } else {
-            res.send('User ' + id + ' deleted!');
-          } 
+            if (err) {
+                res.send({
+                    'error': 'An error has occurred'
+                });
+            } else {
+                res.send('User ' + id + ' deleted!');
+            }
         });
-      });
+    });
 
 
 };
